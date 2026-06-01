0434608737963c4cbf442b432de3a4fc125e5223
Shyammkisshorekolli <kollishyammkisshore5@gmail.com>
2026-06-01 20:05:24 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int>lens;
+    string encoded_string="";
+    vector<string>decoded_strs;
+    int j=0;
+    string encode(vector<string>& strs) {
+        for(int i=0;i<strs.size();i++){
+            lens.push_back(strs[i].length());
+            encoded_string=encoded_string+strs[i];
+        }
+        return encoded_string;
+    }
+
+    vector<string> decode(string s) {
+        for(int i=0;i<lens.size();i++){
+            int k=lens[i];
+            int j_=j+k;
+            string news="";
+            for(;j<j_;j++){
+                news=news+encoded_string[j];
+            }
+            decoded_strs.push_back(news);
+            j=j;
+        }
+        return decoded_strs;
+    }
+};
