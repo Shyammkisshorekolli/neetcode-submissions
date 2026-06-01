393941b9f88b3516987208aa0d4952e87c40b7ce
Shyammkisshorekolli <kollishyammkisshore5@gmail.com>
2026-06-01 12:36:36 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int>maps;
+        int n=nums.size();
+        if(n==0) return false;
+        for(int i=0;i<n;i++){
+            if(maps[nums[i]]==1) return true;
+            else{
+                maps[nums[i]]=maps[nums[i]]+1;
+            }
+        }
+        return false;
+
+    }
+};
